fbad5fb6f8a8175c9d806115b16f1927d3152df1
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2020-12-18 17:07:32 +0500

    View engine

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ const app = express();
 
 app.listen(3000);
 
-// handling requests
+// register view engine
+app.set("view engine", "ejs");
+// if we don't name our html files folder 'views'
+//app.set("views", "folder_name_here");
+
+// handling requests using view engine
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 // 404
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
